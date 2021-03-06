d33a0e485ca6576cbf80cb10c2b01ede8472e9e9
Kristófer <kristoferhhauksson@gmail.com>
2021-03-06 04:05:12 +0000

    When a unique consumable gets clicked, all other consumables of the same type will get unchecked
    E.g. when a weapon oil is clicked, all other weapon oils get unchecked, same for food buffs, battle/guardian elixirs etc.

--- src/js/auras/consumables.js
+++ src/js/auras/consumables.js
@@ -11,33 +11,39 @@ var consumables = {
 	"superManaPotion": {
 		"name": "Super Mana Potion",
 		"iconName": "inv_potion_137",
+		"potion": true,
 	},
 	"destructionPotion": {
 		"name": "Destruction Potion",
 		"iconName": "inv_potion_107",
+		"potion": true,
 	},
 	"blackenedBasilisk": {
 		"name": "Blackened Basilisk",
 		"spellPower": 23,
 		"spirit": 20,
-		"iconName": "inv_misc_food_86_basilisk"
+		"iconName": "inv_misc_food_86_basilisk",
+		"foodBuff": true,
 	},
 	"brilliantWizardOil": {
 		"name": "Brilliant Wizard Oil",
 		"spellPower": 36,
 		"crit": 14,
 		"iconName": "inv_potion_105",
-		"id": 20749
+		"id": 20749,
+		"weaponOil": true,
 	},
 	"superiorWizardOil": {
 		"name": "Superior Wizard Oil",
 		"spellPower": 42,
 		"iconName": "inv_potion_141",
+		"weaponOil": true,
 	},
 	"blessedWizardOil": {
 		"name": "Blessed Wizard Oil",
 		"spellPower": 60,
 		"iconName": "inv_potion_26",
-		"id": 23123
+		"id": 23123,
+		"weaponOil": true,
 	}
 }
--- src/js/ui.js
+++ src/js/ui.js
@@ -25,14 +25,41 @@ for (let consumable of Object.keys(consumables)) {
 	let consumableUl = $("#consumable-list");
 	localStorage[consumable] = localStorage[consumable] || false;
 
-	consumableUl.append($("<li data-checked='" + localStorage[consumable] + "' name='" + consumable + "' id='" + lowerConsumableName + "' class='" + (c.stats ? "stats" : "") + " consumable aura'><a href='https://classic.wowhead.com/item=" + c.id + "'><img alt='" + c.name + "' src='img/" + c.iconName + ".jpg'></a></li>"));
+	consumableUl.append($("<li data-checked='" + localStorage[consumable] + "' name='" + consumable + "' id='" + lowerConsumableName + "' class='" + (c.stats ? "stats " : "") + (c.potion ? "potion " : "") + (c.battleElixir ? "battle-elixir " : "") + (c.guardianElixir ? "guardian-elixir " : "") + (c.weaponOil ? "weapon-oil " : "") + (c.foodBuff ? "food-buff " : "") + "consumable aura'><a href='https://classic.wowhead.com/item=" + c.id + "'><img alt='" + c.name + "' src='img/" + c.iconName + ".jpg'></a></li>"));
 }
 
 // When a buff/debuff/consumable is clicked
 $(".aura").click(function() {
 	let checkedVal = $(this).attr('data-checked') == 'true';
 	$(this).attr('data-checked', !checkedVal);
-	localStorage[$(this).attr("name")] = true;
+	localStorage[$(this).attr("name")] = !checkedVal;
 
 	return false;
 });
+
+// Array of consumables whose clicks we want to track.
+let consumableTypesToTrack = ['.weapon-oil', '.battle-elixir', '.guardian-elixir', '.food-buff', '.potion'];
+// When a consumable is clicked, uncheck all other types of that consumable since we can only have one at a time (e.g. disable all other weapon oils if a weapon oil is clicked).
+$(consumableTypesToTrack.join(',')).click(function(event) {
+	let clickedConsumableName = $(this).attr("name");
+	let consumableTypes = [];
+
+	// Loop through the consumable types we're tracking and check if the consumable that got clicked has any of those consumables as a class.
+	for (let i = 0; i < consumableTypesToTrack.length; i++) {
+		if ($(this).hasClass(consumableTypesToTrack[i].substring(1))) {
+			consumableTypes.push(consumableTypesToTrack[i]);
+		}
+	}
+
+	// Loop through the consumable classes we found in the previous loop and uncheck all childs of those classes aside from the consumable that just got clicked
+	$(consumableTypes.join(',')).each(function() {
+		if ($(this).attr('name') !== clickedConsumableName) {
+			$(this).attr('data-checked', false);
+			localStorage[$(this).attr('name')] = false;
+		}
+	});
+
+	refreshCharacterStats();
+});
+
+refreshCharacterStats();
