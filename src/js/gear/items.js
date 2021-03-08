06bdb2373887571ab1b655d109b2b50058d3c503
Kristófer <kristmundurhh@gmail.com>
2021-03-08 03:25:21 +0000

    Add a few items for each item slot

---
+++ src/js/gear/items.js
@@ -0,0 +1,528 @@
+var items = {
+	"head": {
+		"spellstrikeHood": {
+			"name": "Spellstrike Hood",
+			"stamina": 16,
+			"intellect": 12,
+			"red": 1,
+			"blue": 1,
+			"yellow": 1,
+			"socketBonus": {
+				"stamina": 6
+			},
+			"spellPower": 47,
+			"critRating": 24,
+			"hitRating": 16,
+			"setId": 1, // fix
+			"source": "Tailoring",
+			"phase": 1
+		},
+		"hoodOfOblivion": {
+			"name": "Hood of Oblivion",
+			"stamina": 27,
+			"intellect": 32,
+			"meta": 1,
+			"blue": 1,
+			"socketBonus": {
+				"spellPower": 5
+			},
+			"spellPower": 41,
+			"setId": 1, // fix
+			"source": "Arcatraz",
+			"phase": 1
+		},
+		"evokersHelmetOfSecondSight": {
+			"name": "Evoker's Helmet of Second Sight",
+			"stamina": 12,
+			"intellect": 15,
+			"spirit": 8,
+			"blue": 2,
+			"yellow": 1,
+			"socketBonus": {
+				"spellPower": 5
+			},
+			"spellPower": 36,
+			"critRating": 24,
+			"source": "Shadowmoon Valley Quest",
+			"phase": 1
+		}
+	},
+	"neck": {
+		"hydraFangNecklace": {
+			"name": "Hydra-fang Necklace",
+			"stamina": 17,
+			"intellect": 16,
+			"spellPower": 20,
+			"hitRating": 16,
+			"source": "Heroic Underbog",
+			"phase": 1,
+		},
+		"broochOfHeightenedPotential": {
+			"name": "Brooch of Heightened Potential",
+			"stamina": 15,
+			"intellect": 14,
+			"spellPower": 23,
+			"critRating": 14,
+			"hitRating": 9,
+			"source": "Shadow Labyrinth",
+			"phase": 1,
+		},
+		"natashasEmberNecklace": {
+			"name": "Natasha's Ember Necklace",
+			"intellect": 15,
+			"spellPower": 30,
+			"critRating": 10,
+			"source": "Blade's Edge Mountains Quest",
+			"phase": 1,
+		}
+	},
+	"shoulders": {
+		"frozenShadoweaveShoulders": {
+			"name": "Frozen Shadoweave Shoulders",
+			"stamina": 21,
+			"intellect": 15,
+			"blue": 1,
+			"yellow": 1,
+			"socketBonus": {
+				"hitRating": 3
+			},
+			"shadowPower": 51,
+			"setId": 1, // fix
+			"source": "Tailoring",
+			"phase": 1
+		},
+		"spauldersOfOblivion": {
+			"name": "Spaulders of Oblivion",
+			"stamina": 25,
+			"intellect": 17,
+			"blue": 1,
+			"yellow": 1,
+			"socketBonus": {
+				"hitRating": 3
+			},
+			"spellPower": 30,
+			"source": "Shadow Labyrinth",
+			"phase": 1,
+		},
+		"mantleOfThreeTerrors": {
+			"name": "Mantle of Three Terrors",
+			"stamina": 29,
+			"intellect": 25,
+			"spellPower": 30,
+			"hitRating": 12,
+			"source": "Black Morass",
+			"phase": 1,
+		}
+	},
+	"back": {
+		"sethekkOracleCloak": {
+			"name": "Sethekk Oracle Cloak",
+			"stamina": 18,
+			"intellect": 18,
+			"spellPower": 23,
+			"hitRating": 12,
+			"source": "Sethekk Halls",
+			"phase": 1,
+		},
+		"cloakOfEntropy": {
+			"name": "Cloak of Entropy",
+			"intellect": 11,
+			"spellPower": 26,
+			"hitRating": 10,
+			"source": "BoE World Drop",
+			"phase": 1,
+		},
+		"cloakOfTheBlackVoid": {
+			"name": "Cloak of the Black Void",
+			"intellect": 11,
+			"spellPower": 36,
+			"source": "Tailoring",
+			"phase": 1,
+		}
+	},
+	"chest": {
+		"frozenShadoweaveRobe": {
+			"name": "Frozen Shadoweave Robe",
+			"stamina": 30,
+			"intellect": 20,
+			"blue": 1,
+			"yellow": 1,
+			"socketBonus": {
+				"hitRating": 3
+			},
+			"shadowPower": 73,
+			"source": "Tailoring",
+			"phase": 1
+		},
+		"robeOfTheCrimsonOrder": {
+			"name": "Robe of the Crimson Order",
+			"intellect": 23,
+			"spellPower": 51,
+			"hitRating": 30,
+			"source": "BoE World Drop",
+			"phase": 1,
+		},
+		"auchenaiAnchoritesRobe": {
+			"name": "Auchenai Anchorite's Robe",
+			"intellect": 24,
+			"red": 1,
+			"yellow": 2,
+			"socketBonus": {
+				"critRating": 4
+			},
+			"spellPower": 29,
+			"hitRating": 23,
+			"source": "Auchenai Crypts Quest",
+			"phase": 1
+		}
+	},
+	"bracer": {
+		"crimsonBracersOfGloom": {
+			"name": "Crimson Bracers of Gloom",
+			"stamina": 18,
+			"intellect": 18,
+			"spellPower": 23,
+			"hitRating": 12,
+			"source": "Heroic Hellfire Ramparts",
+			"phase": 1,
+		},
+		"bracersOfHavok": {
+			"name": "Bracers of Havok",
+			"intellect": 12,
+			"yellow": 1,
+			"socketBonus": {
+				"critRating": 2
+			},
+			"spellPower": 31,
+			"source": "Tailoring",
+			"phase": 1,
+		},
+		"shattrathWraps": {
+			"name": "Shattrath Wraps",
+			"stamina": 15,
+			"intellect": 15,
+			"red": 1,
+			"socketBonus": {
+				"stamina": 3
+			},
+			"spellPower": 22,
+			"source": "Auchindon Quest",
+			"phase": 1
+		}
+	},
+	"gloves": {
+		"glovesOfTheDeadwatcher": {
+			"name": "Gloves of the Deadwatcher",
+			"stamina": 24,
+			"intellect": 24,
+			"spellPower": 30,
+			"hitRating": 18,
+			"source": "Heroic Auchenai Crypts",
+			"phase": 1,
+		},
+		"glovesOfOblivion": {
+			"name": "Gloves of Oblivion",
+			"stamina": 33,
+			"intellect": 21,
+			"spellPower": 27,
+			"hitRating": 20,
+			"source": "Shattered Halls",
+			"phase": 1,
+		},
+		"tempestsTouch": {
+			"name": "Tempest's Touch",
+			"stamina": 10,
+			"intellect": 20,
+			"spirit": 6,
+			"blue": 2,
+			"socketBonus": {
+				"critRating": 3
+			},
+			"spellPower": 28,
+			"spellPen": 9,
+			"source": "Caverns of Time Quest",
+			"phase": 1
+		}
+	},
+	"belt": {
+		"girdleOfRuination": {
+			"name": "Girdle of Ruination",
+			"stamina": 18,
+			"intellect": 13,
+			"red": 1,
+			"yellow": 1,
+			"socketBonus": {
+				"stamina": 4
+			},
+			"spellPower": 40,
+			"critRating": 20,
+			"source": "Tailoring",
+			"phase": 1,
+		},
+		"beltOfDepravity": {
+			"name": "Belt of Depravity",
+			"stamina": 31,
+			"intellect": 27,
+			"spellPower": 35,
+			"hitRating": 17,
+			"source": "Heroic Arcatraz",
+			"phase": 1,
+		},
+		"glyphLinedSash": {
+			"name": "Glyph-Lined Sash",
+			"stamina": 21,
+			"intellect": 23,
+			"yellow": 2,
+			"socketBonus": {
+				"spellPower": 4
+			},
+			"spellPower": 31,
+			"critRating": 9,
+			"source": "Heroic Mana-Tombs",
+			"phase": 1,
+		}
+	},
+	"legs": {
+		"spellstrikePants": {
+			"name": "Spellstrike Pants",
+			"stamina": 12,
+			"intellect": 8,
+			"blue": 1,
+			"red": 1,
+			"yellow": 1,
+			"socketBonus": {
+				"stamina": 6,
+			},
+			"spellPower": 47,
+			"critRating": 26,
+			"hitRating": 22,
+			"source": "Tailoring",
+			"phase": 1,
+		},
+		"breechesOfTheOcculist": {
+			"name": "Breeches of the Occulist",
+			"stamina": 37,
+			"intellect": 22,
+			"yellow": 2,
+			"blue": 1,
+			"socketBonus": {
+				"spellPower": 5
+			},
+			"spellPower": 37,
+			"critRating": 23,
+			"source": "Heroic Black Morass",
+			"phase": 1,
+		},
+		"khadgarsKiltOfAbjuration": {
+			"name": "Khadgar's Kilt of Abjuration",
+			"stamina": 20,
+			"intellect": 22,
+			"spirit": 15,
+			"blue": 2,
+			"yellow": 1,
+			"socketBonus": {
+				"spellPower": 5
+			},
+			"spellPower": 37,
+			"source": "Black Morass",
+			"phase": 1,
+		}
+	},
+	"boots": {
+		"frozenShadoweaveBoots": {
+			"name": "Frozen Shadoweave Boots",
+			"stamina": 15,
+			"intellect": 9,
+			"blue": 1,
+			"yellow": 1,
+			"socketBonus": {
+				"hitRating": 3
+			},
+			"shadowPower": 58,
+			"source": "Tailoring",
+			"phase": 1,
+		},
+		"shattrathJumpers": {
+			"name": "Shattrath Jumpers",
+			"stamina": 25,
+			"intellect": 17,
+			"blue": 1,
+			"yellow": 1,
+			"socketBonus": {
+				"intellect": 3
+			},
+			"spellPower": 30,
+			"source": "Auchindon Quest",
+			"phase": 1,
+		},
+		"extravagantBootsOfMalice": {
+			"name": "Extravagant Boots of Malice",
+			"stamina": 27,
+			"intellect": 24,
+			"spellPower": 31,
+			"hitRating": 14,
+			"source": "Heroic Mana-Tombs",
+			"phase": 1,
+		}
+	},
+	"ring": {
+		"ashyensGift": {
+			"name": "Ashyen's Gift",
+			"stamina": 30,
+			"spellPower": 24,
+			"hitRating": 21,
+			"source": "Cenarion Expedition - Exalted",
+			"phase": 1,
+		},
+		"sparkingArcaniteRing": {
+			"name": "Sparking Arcanite Ring",
+			"stamina": 13,
+			"intellect": 14,
+			"spellPower": 23,
+			"critRating": 14,
+			"hitRating": 10,
+			"source": "Heroic Old Hillsbrad Foothills",
+			"phase": 1,
+		}
+	},
+	"trinket": {
+		"darkmoonCardCrusade": {
+			"name": "Darkmoon Card: Crusade",
+			"source": "Blessings Deck",
+			"phase": 2, // unsure?
+		},
+		"iconOfTheSilverCrescent": {
+			"name": "Icon of the Silver Crescent",
+			"spellPower": 44,
+			"onUseSpellPower": 156,
+			"duration": 20,
+			"cooldown": 120,
+			"source": "41 Badge of Justice - G'eras",
+			"phase": 1,
+		}
+	},
+	"mainhand": {
+		"bladeOfTheArchmage": {
+			"name": "Blade of the Archmage",
+			"stamina": 13,
+			"intellect": 11,
+			"spellPower": 160,
+			"critRating": 21,
+			"source": "Honor Hold/Thrallmar - Exalted",
+			"phase": 1,
+		},
+		"bladeOfWizardry": {
+			"name": "Blade of Wizardry",
+			"spellPower": 160,
+			"source": "BoE World Drop",
+			"phase": 11
+		},
+		"greatswordOfHorridDreams": {
+			"name": "Greatsword of Horrid Dreams",
+			"stamina": 15,
+			"intellect": 14,
+			"spellPower": 131,
+			"hitRating": 14,
+			"source": "Shadow Labyrinth",
+			"phase": 1
+		}
+	},
+	"offhand": {
+		"orbOfTheSoulEater": {
+			"name": "Orb of the Soul-Eater",
+			"stamina": 18,
+			"shadowPower": 52,
+			"source": "25 Badge of Justice - G'eras",
+			"phase": 1,
+		},
+		"lampOfPeacefulRadiance": {
+			"name": "Lamp of Peaceful Radiance",
+			"stamina": 13,
+			"intellect": 14,
+			"spellPower": 22,
+			"critRating": 13,
+			"hitRating": 12,
+			"source": "Arcatraz",
+			"phase": 1
+		},
+		"starHeartLamp": {
+			"name": "Star-Heart Lamp",
+			"stamina": 17,
+			"intellect": 18,
+			"spellPower": 23,
+			"hitRating": 12,
+			"source": "Black Morass",
+			"phase": 1,
+		}
+	},
+	"twohand": {
+		"terokksShadowstaff": {
+			"name": "Terokk's Shadowstaff",
+			"stamina": 40,
+			"intellect": 42,
+			"spellPower": 169,
+			"critRating": 37,
+			"source": "Heroic Sethekk Halls",
+			"phase": 1,
+		},
+		"auchenaiStaff": {
+			"name": "Auchenai Staff",
+			"intellect": 46,
+			"spellPower": 122,
+			"hitRating": 19,
+			"critRating": 26,
+			"source": "The Aldor - Revered",
+			"phase": 1
+		},
+		"warpstaffOfArcanum": {
+			"name": "Warpstaff of Arcanum",
+			"stamina": 37,
+			"intellect": 38,
+			"spellPower": 122,
+			"critRating": 26,
+			"hitRating": 16,
+			"source": "Botanica",
+			"phase": 1
+		}
+	},
+	"wand": {
+		"netherCoresControlRod": {
+			"name": "Nether Core's Control Rod",
+			"damageLower": 163,
+			"damageUpper": 303,
+			"speed": 1.8,
+			"stamina": 9,
+			"intellect": 10,
+			"spellPower": 14,
+			"hitRating": 8,
+			"source": "Arcatraz",
+			"phase": 1,
+		},
+		"illidariRodOfDiscipline": {
+			"name": "Illidari Rod of Discipline",
+			"damageLower": 107,
+			"damageUpper": 199,
+			"speed": 1.4,
+			"stamina": 9,
+			"blue": 1,
+			"socketBonus": {
+				"spellPower": 2
+			},
+			"spellPower": 14,
+			"source": "Shadowmoon Valley Quest",
+			"phase": 1
+		},
+		"voidfireWand": {
+			"name": "Voidfire Wand",
+			"damageLower": 138,
+			"damageUpper": 257,
+			"speed": 1.9,
+			"stamina": 9,
+			"intellect": 9,
+			"spellPower": 12,
+			"hitRating": 7,
+			"source": "Mana-Tombs",
+			"phase": 1
+		}
+	}
+}
