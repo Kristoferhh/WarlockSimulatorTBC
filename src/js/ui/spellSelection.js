b9db470429584b3a1064de4cc523a46c9f5156d4
Kristófer <kristmundurhh@gmail.com>
2021-03-19 17:01:10 +0000

    Rewrote the rotation selection into a JS class rather than hardcoded HTML

---
+++ src/js/ui/spellSelection.js
@@ -0,0 +1,87 @@
+var _spellSelection = {
+	"dot": {
+		"header": "Dots",
+		"spells": {
+			"immolate": {
+				"name": "Immolate",
+				"iconName": "spell_fire_immolation",
+				"id": 25309,
+			},
+			"corruption": {
+				"name": "Corruption",
+				"iconName": "spell_shadow_abominationexplosion",
+				"id": 25311,
+			},
+			"siphonLife": {
+				"name": "Siphon Life",
+				"iconName": "spell_shadow_requiem",
+				"id": 18881,
+			},
+			"unstableAffliction": {
+				"name": "Unstable Affliction",
+				"iconName": "spell_shadow_unstableaffliction_3",
+				"id": 30108,
+			}
+		}
+	},
+	"filler": {
+		"header": "Filler",
+		"spells": {
+			"searingPain": {
+				"name": "Searing Pain",
+				"iconName": "spell_fire_soulburn",
+				"id": 17923,
+			},
+			"shadowBolt": {
+				"name": "Shadow Bolt",
+				"iconName": "spell_shadow_shadowbolt",
+				"id": 25307,
+			},
+			"incinerate": {
+				"name": "Incinerate",
+				"iconName": "spell_fire_burnout",
+				"id": 29722,
+			}
+		}
+	},
+	"curse": {
+		"header": "Curses",
+		"spells": {
+			"curseOfRecklessness": {
+				"name": "Curse of Recklessness",
+				"iconName": "spell_shadow_unholystrength",
+				"id": 11717,
+			},
+			"curseOfTheElements": {
+				"name": "Curse of the Elements",
+				"iconName": "spell_shadow_chilltouch",
+				"id": 11722,
+			},
+			"curseOfDoom": {
+				"name": "Curse of Doom",
+				"iconName": "spell_shadow_auraofdarkness",
+				"id": 603,
+			},
+			"curseOfAgony": {
+				"name": "Curse of Agony",
+				"iconName": "spell_shadow_curseofsargeras",
+				"id": 11713,
+			}
+		}
+	},
+	"finisher": {
+		"header": "Finishers",
+		"spells": {
+			"deathCoil": {
+				"name": "Death Coil",
+				"iconName": "spell_shadow_deathcoil",
+				"id": 17926,
+			},
+			"shadowburn": {
+				"name": "Shadowburn",
+				"iconName": "spell_shadow_scourgebuild",
+				"id": 18871,
+			}
+		}
+	}
+}
