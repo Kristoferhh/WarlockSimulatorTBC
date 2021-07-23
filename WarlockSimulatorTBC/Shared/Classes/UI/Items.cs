4d49e30f8d4d2d5959824b5ccc75b20e0566989f
Kristófer <kristmundurhh@gmail.com>
2021-07-23 19:13:54 +0000

    Combined items and enchants into the same razor component

--- WarlockSimulatorTBC/Shared/Classes/UI/Items.cs
+++ WarlockSimulatorTBC/Shared/Classes/UI/Items.cs
@@ -7,6 +7,7 @@ namespace WarlockSimulatorTBC.Shared.Classes.UI
 {
 	public static class ItemSource
 	{
+		public static string GRUULS_LAIR = "Gruul's Lair";
 		public static string KARAZHAN = "Karazhan";
 		public static string TAILORING = "Tailoring";
 		public static string NAXXRAMAS = "Naxxramas";
@@ -73,15 +74,12 @@ namespace WarlockSimulatorTBC.Shared.Classes.UI
 
 	public static class Items
 	{
-		public static void LoadItemsBySlot(string itemSlot)
-		{
-			Console.WriteLine("Loading item slot " + itemSlot);
-		}
+		public static string SelectedItemSlot { get; set; } = ItemSlots.HEAD;
 
 		public static readonly Dictionary<string, ItemSlot> itemSlots = new Dictionary<string, ItemSlot>
 		{
 			{
-				"head", new ItemSlot
+				ItemSlots.HEAD, new ItemSlot
 				{
 					items = new Dictionary<string, Item>
 					{
@@ -123,6 +121,50 @@ namespace WarlockSimulatorTBC.Shared.Classes.UI
 								spellHitRating = 16,
 								setId = 559,
 								source = ItemSource.TAILORING
+							}
+						}
+					}
+				}
+			},
+			{
+				ItemSlots.NECK, new ItemSlot
+				{
+					items = new Dictionary<string, Item>
+					{
+						{
+							"adornmentOfStolenSouls", new Item
+							{
+								name = "Adornment of Stolen Souls",
+								stamina = 18,
+								intellect = 20,
+								spellPower = 28,
+								spellCritRating = 23,
+								id = 28762,
+								source = ItemSource.KARAZHAN,
+								phase = 1
+							}
+						}
+					}
+				}
+			},
+			{
+				ItemSlots.SHOULDERS, new ItemSlot
+				{
+					items = new Dictionary<string, Item>
+					{
+						{
+							"voidheartMantle", new Item
+							{
+								name = "Voidheart Mantle",
+								stamina = 26,
+								intellect = 22,
+								spellPower = 37,
+								spellHitRating = 14,
+								blue = 1,
+								yellow = 1,
+								id = 28967,
+								source = ItemSource.GRUULS_LAIR,
+								phase = 1
 							}
 						}
 					}
