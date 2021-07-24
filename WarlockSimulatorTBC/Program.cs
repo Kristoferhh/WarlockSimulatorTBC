c746612b08679cb94c5fed41361a5f11346bf4c9
Kristófer <kristmundurhh@gmail.com>
2021-07-24 23:44:35 +0000

    Add Blazored.LocalStorage

--- WarlockSimulatorTBC/Program.cs
+++ WarlockSimulatorTBC/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
 
 namespace WarlockSimulatorTBC
 {
@@ -18,6 +19,7 @@ namespace WarlockSimulatorTBC
 			builder.RootComponents.Add<App>("#app");
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddBlazoredLocalStorage();
 
 			await builder.Build().RunAsync();
 		}
