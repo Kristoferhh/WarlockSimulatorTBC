c6a9bcaacece3532a7876ac6d2b638516a802571
Kristófer <kristmundurhh@gmail.com>
2021-09-12 22:06:56 +0000

    Fix infinite sim error

--- src/js/classes/player.js
+++ src/js/classes/player.js
@@ -540,8 +540,9 @@ class Player {
   }
 
   cast (spell, predictedDamage = 0) {
-    if (this.gcdRemaining > 0 && this.spells[spell].onGcd)
+    if (this.gcdRemaining > 0 && this.spells[spell].onGcd) {
       //throw 'Attempting to cast a spell (' + spell + ') on GCD with ' + this.gcdRemaining + ' cooldown remaining.'
+    }
     this.spells[spell].startCast(predictedDamage)
   }
 
