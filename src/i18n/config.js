0b3074e4af630ff25be69738627b9228998b2ba6
Kristófer <kristmundurhh@gmail.com>
2021-11-21 08:03:23 +0000

    Attempt to set the website's default language to the user's browser language

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,9 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { Languages } from '../Types';
 
 i18n.use(initReactI18next).init({
   fallbackLng: 'en',
-  lng: localStorage.getItem('language') || 'en',
+  lng: localStorage.getItem('language') ||
+    (navigator.languages.length > 0 &&
+      navigator.languages[0].split('-').length > 0 &&
+      Languages.find(e => e.iso === navigator.languages[0].split('-')[0]) ?
+      navigator.languages[0].split('-')[0] : 'en'),
   resources: {
     zh: {
       translations: require('./locales/zh/translations.json')
