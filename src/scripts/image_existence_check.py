9ae5cd59ba37ecb992b5dc66846b073f33468a62
Kristófer <kristoferhh@hotmail.com>
2022-05-16 18:41:20 +0000

    Fix image existence script

--- src/scripts/image_existence_check.py
+++ src/scripts/image_existence_check.py
@@ -2,7 +2,9 @@ import os
 
 
 def get_icon_name_from_string(string: str):
-    return string[string.find('iconName') + len('iconName: "'):len(string) - 3]
+    start_index = string.find('iconName') + len('iconName: "')
+    end_index = string.find(',', start_index) - 1
+    return string[start_index:end_index]
 
 def assert_icon_image_existence_in_file(file_path: str):
     with open(file_path) as f:
